9e3c0da5a3b111e120fd3b3af95cbe90c621c148
SBKF19 <sofiane.benkherouf05@gmail.com>
2026-03-04 17:22:28 +0100

    Ajout des formulaires de login avec sécurité

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
